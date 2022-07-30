e9c1b1a79f5c97c961133e74396a5bac7b89781c
tranthanhtruc413 <21521746@gm.uit.edu.vn>
2022-07-30 18:17:15 +0700

    Update database

--- food-order.sql
+++ food-order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2022 at 05:42 PM
+-- Generation Time: Jul 30, 2022 at 01:16 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -43,7 +43,7 @@ INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
 (6, 'thang', 'thang', '6e0c130ca8cf53a2473bd88044b83da9'),
 (7, 'khanh', 'khanh', '46c9a651ec34e9118b64e72ae13b067f'),
 (8, 'huy', 'huy', '11967d5e9addc5416ea9224eee0e91fc'),
-(9, '1', '1', 'c4ca4238a0b923820dcc509a6f75849b');
+(10, '1', '1', 'c4ca4238a0b923820dcc509a6f75849b');
 
 -- --------------------------------------------------------
 
@@ -54,10 +54,22 @@ INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
 CREATE TABLE `tbl_category` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
-  `image-name` varchar(255) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
   `featured` varchar(10) NOT NULL,
   `active` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_category`
+--
+
+INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
+(8, 'Pizza ', 'Food_Category_505.jpg', 'Yes', 'Yes'),
+(12, 'Burger', 'Food_Category_950.jpg', 'Yes', 'Yes'),
+(15, 'Momo', 'Food_Category_509.jpg', 'Yes', 'Yes'),
+(16, 'dsd', '', 'No', 'No'),
+(17, 'dsa', '', 'Yes', 'No'),
+(18, 'test', '', 'No', 'No');
 
 -- --------------------------------------------------------
 
@@ -75,6 +87,15 @@ CREATE TABLE `tbl_food` (
   `featured` varchar(10) NOT NULL,
   `active` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_food`
+--
+
+INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
+(5, 'Best Burger', 'Burger with Ham, Pineapple and lots of Cheese.', '4.00', 'Food-name-5759.jpg', 12, 'Yes', 'Yes'),
+(6, 'Dumplings Specical', 'Chicken Dumpling with herbs from Moutains', '5.00', 'Food-name-1613.jpg', 15, 'Yes', 'Yes'),
+(7, 'Smoky BBQ Pizza', 'Best Firewood Pizza in Town', '6.00', 'Food-name-2672.jpg', 8, 'Yes', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -95,6 +116,15 @@ CREATE TABLE `tbl_order` (
   `customer_email` varchar(255) NOT NULL,
   `customer_address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
+(3, 'Best Burger', '4.00', 19, '76.00', '2022-07-30 12:21:48', 'On Delivery', 'dsa', '3123213', 'dasdasd@gmail.com', 'dasdas'),
+(4, 'Best Burger', '4.00', 2, '8.00', '2022-07-30 12:22:08', 'Delivered', 'dasda', '3123213213213', 'dasdasd@gmai.com', 'dasdsadsad'),
+(5, 'Smoky BBQ Pizza', '6.00', 7, '42.00', '2022-07-30 12:25:00', 'Canceled', 'đá', '021564', 'dasdadsds@gmail.com', '1dddasdd');
 
 --
 -- Indexes for dumped tables
@@ -132,25 +162,25 @@ ALTER TABLE `tbl_order`
 -- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tbl_food`
 --
 ALTER TABLE `tbl_food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
