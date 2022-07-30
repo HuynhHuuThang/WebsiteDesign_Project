ad907884fc3d48598a2e93289b6589dec8005800
tranthanhtruc413 <21521746@gm.uit.edu.vn>
2022-07-30 12:05:26 +0700

    Delete and Update Food

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -5,16 +5,46 @@
         <h1> Manage Food</h1>
         <?php
         
-            if(isset($_SESSION['add']))
-            {
-                echo $_SESSION['add'];
-                unset($_SESSION['add']);
-            }
-            if(isset($_SESSION['upload']))
-            {
-                echo $_SESSION['upload'];
-                unset($_SESSION['upload']);
-            }
+        if(isset($_SESSION['add']))
+        {
+            echo $_SESSION['add'];
+            unset($_SESSION['add']);
+        }
+        if(isset($_SESSION['remove']))
+        {
+            echo $_SESSION['remove'];
+            unset($_SESSION['remove']);
+        }
+        if(isset($_SESSION['delete']))
+        {
+            echo $_SESSION['delete'];
+            unset($_SESSION['delete']);
+        }
+        if(isset($_SESSION['no-food-found']))
+        {
+            echo $_SESSION['no-food-found'];
+            unset($_SESSION['no-food-found']);
+        }
+        if(isset($_SESSION['update']))
+        {
+            echo $_SESSION['update'];
+            unset($_SESSION['update']);
+        }
+        if(isset($_SESSION['upload']))
+        {
+            echo $_SESSION['upload'];
+            unset($_SESSION['upload']);
+        }
+        if(isset($_SESSION['failed-remove']))
+        {
+            echo $_SESSION['failed-remove'];
+            unset($_SESSION['failed-remove']);
+        }
+        if(isset($_SESSION['unauthorize']))
+        {
+            echo $_SESSION['unauthorize'];
+            unset($_SESSION['unauthorize']);
+        }
         ?>
         <br /><br />
             <!-- button add admin -->
@@ -62,15 +92,15 @@
                                         else
                                         {
                                             ?>
-                                            <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>">
+                                            <img width="250px" src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>">
                                             <?php
                                         }
                                     ?></td>
                                 <td><?php echo $featured; ?></td>             
                                 <td><?php echo $active; ?></td>
                                 <td>
-                                    <a href="#" class="btn-secondary">Update Food</a>
-                                    <a href="#" class="btn-danger">Delete Food</a>
+                                    <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Update Food</a>
+                                    <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a>
                                 </td>
                             </tr>
                         <?php
