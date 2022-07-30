2240e7720f68dd64f07d65752775f476e9d88259
tranthanhtruc413 <21521746@gm.uit.edu.vn>
2022-07-30 10:58:50 +0700

    Adding and Displaying Foods

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -3,49 +3,86 @@
 <div class="main-content">
     <div class="wrapper">
         <h1> Manage Food</h1>
-
+        <?php
+        
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add'];
+                unset($_SESSION['add']);
+            }
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload'];
+                unset($_SESSION['upload']);
+            }
+        ?>
         <br /><br />
             <!-- button add admin -->
-            <a href="" class=""btn-primary>add admin</a>
+            <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
 
             <br /><br />
             <table class="tbl-full">
                 <tr>
                     <th>S.N.</th>
-                    <th>Full name</th>
-                    <th>Username</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Featured</th>
+                    <th>Active</th>
                     <th>Action</th>
                 </tr>
 
-                <tr>
-                    <td>1.</td>
-                    <td>Huynh Huu Thang</td>
-                    <td>HuynhHuuThang</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>2.</td>
-                    <td>Huynh Huu Thang</td>
-                    <td>HuynhHuuThang</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>3.</td>
-                    <td>Huynh Huu Thang</td>
-                    <td>HuynhHuuThang</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
+                <?php
+                $sql = "SELECT * FROM tbl_food";
+                $res = mysqli_query($conn,$sql);
+                $count = mysqli_num_rows($res);
+                $sn=1;
+                if($count>0)
+                {
+                    while($row=mysqli_fetch_assoc($res))
+                    {
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $price = $row['price'];
+                        $image_name = $row['image_name'];
+                        $featured = $row['featured'];
+                        $active = $row['active'];
+                        
+                        ?>
+                                <tr>
+                                <td><?php echo $sn++; ?>.</td>
+                                <td><?php echo $title; ?></td>
+                                <td>$<?php echo $price; ?></td>
+                                <td>
+                                    <?php 
+                                        if($image_name=="")
+                                        {
+                                            echo "<div class='error'> Image not Added.</div>"; 
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                            <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>">
+                                            <?php
+                                        }
+                                    ?></td>
+                                <td><?php echo $featured; ?></td>             
+                                <td><?php echo $active; ?></td>
+                                <td>
+                                    <a href="#" class="btn-secondary">Update Food</a>
+                                    <a href="#" class="btn-danger">Delete Food</a>
+                                </td>
+                            </tr>
+                        <?php
+                    }
+                }
+                else
+                {
+                    echo "<tr><td colspan='7' class='error'>Food not Added Yet.</td></tr>";
+                }
+                ?>
+        
+               
             </table>
     </div>
 </div>
