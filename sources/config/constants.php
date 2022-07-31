584c2a3348019561486f996a2c73e9e2bd59a947
KhanhVu2002 <20520979@gm.uit.edu.vn>
2022-07-31 11:35:37 +0700

    up

--- sources/config/constants.php
+++ sources/config/constants.php
@@ -1,12 +1,12 @@
 <?php
         //start sessions
         session_start();
-        
-        define('SITEURL','http://localhost/');
-        define('LOCALHOST','localhost');
-        define('DB_USERNAME','root');
-        define('DB_PASSWORD','123');
+        define('LOCALHOST','db');
+        define('DB_USERNAME','admin');
+        define('DB_PASSWORD','uit');
         define('DB_NAME','food-order');
+        define('DB_PORT', "3306");
+
         // create constants to store non repeating values
         $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die (mysqli_error()); // Database connection
         $db_select = mysqli_select_db($conn,DB_NAME) or die (mysqli_error()); // Database selection
